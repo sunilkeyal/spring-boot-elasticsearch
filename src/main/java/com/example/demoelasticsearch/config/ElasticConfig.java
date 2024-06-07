801c1a94f27936c27f5b80c9591a5723d804788d
Sunil Keyal <sunilkeyal@hotmail.com>
2024-06-07 09:39:29 -0400

    updated app versions

--- src/main/java/com/example/demoelasticsearch/config/ElasticConfig.java
+++ src/main/java/com/example/demoelasticsearch/config/ElasticConfig.java
@@ -1,19 +1,17 @@
 package com.example.demoelasticsearch.config;
 
-import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
-import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+
 
 /**
  * When configuring Elasticsearch in our Java application, we need to define how we connect to the Elasticsearch instance.
- * For that, we use a RestHighLevelClient that is offered by the Elasticsearch dependency
  *
  */
 @Configuration
-public class ElasticConfig {
+public class ElasticConfig extends ElasticsearchConfiguration {
 
     @Value("${demo.elasticsearch.host}")
     private String host;
@@ -21,14 +19,11 @@ public class ElasticConfig {
     @Value(("${demo.elasticsearch.port}"))
     private String port;
 
-    @Bean
-    public RestHighLevelClient client() {
-        ClientConfiguration clientConfiguration
-                = ClientConfiguration.builder()
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
                 .connectedTo(host + ":" + port)
                 .build();
-
-        return RestClients.create(clientConfiguration).rest();
     }
 }
 
